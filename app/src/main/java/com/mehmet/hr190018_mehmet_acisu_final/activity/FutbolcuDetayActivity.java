98cac20f757f29f05aa082cd75a9fefed9004d79
Mehmet ACISU <61197669+mehmetacisuu@users.noreply.github.com>
2021-01-18 20:58:41 +0300

    FootballerListApp

--- app/src/main/java/com/mehmet/hr190018_mehmet_acisu_final/activity/FutbolcuDetayActivity.java
+++ app/src/main/java/com/mehmet/hr190018_mehmet_acisu_final/activity/FutbolcuDetayActivity.java
@@ -1,6 +1,8 @@
 package com.mehmet.hr190018_mehmet_acisu_final.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,7 +28,7 @@ public class FutbolcuDetayActivity extends AppCompatActivity {
     private void init(){
         String tasinanFutbolcuString = getIntent().getStringExtra(Constants.TIKLANAN_FUTBOLCU_BASLIK);
         FutbolcuModel futbolcuModel = ObjectUtil.jsonStringToFutbolcu(tasinanFutbolcuString);
-
+        
         imgDetay = findViewById(R.id.imgDetay);
         txtBaslik = findViewById(R.id.txtBaslik);
         txtDetay = findViewById(R.id.txtDetay);
